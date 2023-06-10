3860c54b87456d341eeefa33757bdede1be86791
mscasallasm <stiven_casallas@hotmail.com>
2023-06-10 10:03:21 -0500

    Clase 6 Metodo constructor: Se aprende que es un metodo constructor, el cual ayuda a construir los objetos, las instancias.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Maik");
         myDoctor.name = "Pepe";
         myDoctor.showName();
 
