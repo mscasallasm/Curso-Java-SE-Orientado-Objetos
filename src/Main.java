d2ef40b01ac9ce667ce9ee7cecf477685073b305
mscasallasm <stiven_casallas@hotmail.com>
2023-06-10 10:42:05 -0500

    Clase 9 Final- Varibles constantes: Se aprendio como usar Static, en que casos, no siempre va de la mano con Final, Final, es para declarar constantes.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Pepe";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
         showMenu();
 
     }
