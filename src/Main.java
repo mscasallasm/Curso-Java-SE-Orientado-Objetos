b1987bc764a607f9e4dc9500a25ec46815e109e5
mscasallasm <stiven_casallas@hotmail.com>
2023-06-10 10:31:17 -0500

    Clase 8 Creando elementos estáticos: Basicamente static permite persistir informació en cualquier lugar de la aplicación.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Maik");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Pepe";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
 
     }
 }
