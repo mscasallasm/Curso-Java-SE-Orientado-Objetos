5ce2896a1c05478d526270f0c75816d9901bb9db
mscasallasm <stiven_casallas@hotmail.com>
2023-06-10 10:53:03 -0500

    Clase 10 Sobrecarga de métodos y constructores: Se pueden crear varios metodos con el mismo nombre, lo que cambia es lo que resive y lo que devuelve, también se puede realizar con el constructor.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
 
+        Doctor myDoctor = new Doctor("Maik", "Master");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
