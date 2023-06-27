8492dd9970e0b5a78e77a73e0d1191cd22c92060
mscasallasm <stiven_casallas@hotmail.com>
2023-06-27 11:02:01 -0500

    Clase 15 Variable vs. Objeto: Un vistazo a la memoria, hay dos tipos de memoria la stack y la heap, los datos primitivos se guardan directamente en la stack, cuando se crea un objeto en stack se guarda es el lugar de la ubicación de heap, cuando un objeto se le asigna otro lo que hace es asignarle donde esta asignado, por eso si se hace una modificación se ve reflejado en ambos.

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,25 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient = new Patient("Stiven", "stiven@hotmail.com");
-        patient.setWeight(54.6);
+        Patient patient2 = new Patient("Yulie", "Yulie@hotmail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2.setName("Pepe");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+
+        /*patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
 
 
     }
