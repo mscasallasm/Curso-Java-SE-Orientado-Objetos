9283e6c316b15b301e5d26e2f5a3189a4cdab168
mscasallasm <stiven_casallas@hotmail.com>
2024-04-25 11:10:54 -0500

    Clase 22: Polimorfismo: Sobreescribiendo el método toString

--- src/Doctor.java
+++ src/Doctor.java
@@ -28,6 +28,11 @@ public class Doctor extends User {
         return availableAppointments;
     }
 
+    @Override
+    public String toString() {
+        return super.toString() + "\nSpeciality: " + speciality + "\nAvailable: " + availableAppointments.toString();
+    }
+
     public static class AvailableAppointment{
         private int id;
         private Date date;
@@ -60,6 +65,11 @@ public class Doctor extends User {
 
         public void setTime(String time) {
             this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Available Appointments \nDate: " + date + "\nTime: " + time;
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,21 @@ public class Main {
 
         //showMenu();
 
-//        Doctor myDoctor = new Doctor("Maik", "Master");
-//        myDoctor.addAvailableAppointment(new Date(),"4pm");
-//        myDoctor.addAvailableAppointment(new Date(),"10am");
-//        myDoctor.addAvailableAppointment(new Date(),"1pm");
-//
-//        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+        Doctor myDoctor = new Doctor("Maik", "Master");
+        myDoctor.addAvailableAppointment(new Date(),"4pm");
+        myDoctor.addAvailableAppointment(new Date(),"10am");
+        myDoctor.addAvailableAppointment(new Date(),"1pm");
+
+        System.out.println(myDoctor);
+
+        //        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
 //            System.out.println(aA.getDate() + " " + aA.getTime());
 //        }
 
 
 
         Patient patient = new Patient("Stiven", "stiven@hotmail.com");
-        System.out.println(patient);
+//        System.out.println(patient);
 
 
 
